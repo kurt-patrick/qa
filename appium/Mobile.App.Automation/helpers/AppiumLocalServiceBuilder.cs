3929362c9aca29e607d6e5f6abedabd23353693b
kp <kurt_patrick_online@hotmail.com>
2017-08-26 12:18:53 +0800

    Added process helper to check if android device is running. Also refactored AppiumLocalServiceBuilder

---
+++ appium/Mobile.App.Automation/helpers/AppiumLocalServiceBuilder.cs
@@ -0,0 +1,56 @@
+﻿using KPE.Mobile.App.Automation.Configuration;
+using KPE.Mobile.App.Automation.QA;
+using NUnit.Framework;
+using OpenQA.Selenium.Appium.Service;
+using OpenQA.Selenium.Appium.Service.Options;
+using System;
+
+namespace KPE.Mobile.App.Automation.Helpers
+{
+    public sealed class AppiumLocalServiceBuilder
+    {
+        public AppiumLocalService LocalService { get; private set; } = null;
+
+        private AppiumLocalServiceBuilder(AppiumLocalService appiumLocalService)
+        {
+            LocalService = appiumLocalService;
+        }
+
+        public static AppiumLocalServiceBuilder Build(DriverCapabilities capabilities)
+        {
+            ObjectQA.ThrowIfNull(capabilities);
+
+            var serverOptions = new OptionCollector();
+            serverOptions.AddCapabilities(capabilities.DesiredCapabilities());
+
+            var appiumLocalService = 
+                new AppiumServiceBuilder()
+                    .UsingAnyFreePort()
+                    .WithArguments(serverOptions)
+                    .WithIPAddress("127.0.0.1")
+                    .WithStartUpTimeOut(TimeSpan.FromSeconds(10))
+                    .Build();
+
+            return new AppiumLocalServiceBuilder(appiumLocalService);
+        }
+
+        public AppiumLocalServiceBuilder Start()
+        {
+            ObjectQA.ThrowIfNull(LocalService);
+            if (!LocalService.IsRunning)
+            {
+                LocalService.Start();
+            }
+            return this;
+        }
+
+        public AppiumLocalService AssertIsRunning(int timeout)
+        {
+            ObjectQA.ThrowIfNull(LocalService);
+            WaitHelper.TryWaitForCondition(() => LocalService.IsRunning, timeout);
+            Assert.IsTrue(LocalService.IsRunning, "Appium local service is not running");
+            return LocalService;
+        }
+
+    }
+}
