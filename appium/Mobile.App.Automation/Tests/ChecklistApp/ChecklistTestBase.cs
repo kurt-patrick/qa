45dc47c2d2fe0eb0ab6caf815a78580f506d3042
kp <kurt_patrick_online@hotmail.com>
2018-03-15 20:41:05 +0800

    added applitools eyes validation to checklist tests

--- appium/Mobile.App.Automation/Tests/ChecklistApp/ChecklistTestBase.cs
+++ appium/Mobile.App.Automation/Tests/ChecklistApp/ChecklistTestBase.cs
@@ -1,4 +1,6 @@
-﻿using KPE.Mobile.App.Automation.Configuration;
+﻿using Applitools.Appium;
+using KPE.Mobile.App.Automation.Configuration;
+using KPE.Mobile.App.Automation.Helpers;
 using KPE.Mobile.App.Automation.PageObjects.ChecklistApp;
 using NUnit.Framework;
 using System.Collections.Generic;
@@ -8,6 +10,10 @@ namespace KPE.Mobile.App.Automation.Tests.ChecklistApp
     [TestFixtureSource("CapabilitiesList")]
     internal class ChecklistTestBase : TestBaseGeneric<MainPage>
     {
+        const string AppName = "ChecklistApp";
+
+        Eyes _eyes = null;
+
         public ChecklistTestBase(DriverCapabilities caps) : base(caps)
         {
         }
@@ -16,6 +22,18 @@ namespace KPE.Mobile.App.Automation.Tests.ChecklistApp
         {
             return DriverCapabilities.ChecklistApp();
         }
+
+        protected Eyes GetEyes(string testName)
+        {
+            if(_eyes == null)
+            {
+                _eyes = new Eyes();
+                _eyes.ApiKey = EnvironmentHelper.GetEnvVariable("ApplitoolsEyesApiKey");
+                _eyes.Open(_driver, AppName, testName);
+            }
+            return _eyes;
+        }
+
 
     }
 }
